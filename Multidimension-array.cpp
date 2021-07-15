e5cf6d7b3a6bd9c471ce11abdca3570b715e7c20
Virtual Academy of Pakistan <82775606+virtualacademy-pk@users.noreply.github.com>
2021-07-15 11:58:35 +0300

    Update Multidimension-array.cpp

--- Multidimension-array.cpp
+++ Multidimension-array.cpp
@@ -1,26 +1,42 @@
 
 #include <iostream>
 using namespace std;
+
 int main()
 {
+    
     int x[3][2];
     x[0][0] = 89;
     x[0][1] = 80;
+
     x[1][0] = 92;
     x[1][1] = 71;
+
     x[2][0] = 209;
     x[2][1] = 78;
+
     //int x[3][2] = {{0,1}, {2,3}, {4,5}};
   
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 2; j++)
+        {
+            cout << "x[" << i<< "][" << j << "]: ";
+            cin >> x[i][j];
+        }
+    } 
+    
+    cout<<"Print array";
     
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 2; j++)
         {
-            cout << "Element at x[" << i
-                 << "][" << j << "]: ";
+            cout << "Element at x[" << i<< "][" << j << "]: ";
             cout << x[i][j]<<endl;
         }
     } 
     return 0;
 }
+
+
